a908b74fa81a6e05fb590c8f92c6b917b57d89e5
Erick Christian <erickchristian48@gmail.com>
2026-02-01 11:21:25 +0700

    runtime

--- apps/api/src/lib/runtime.ts
+++ apps/api/src/lib/runtime.ts
@@ -1,12 +1,19 @@
 import { drizzle } from "drizzle-orm/d1"
-import { Layer, Schema } from "effect"
+import { Layer, ManagedRuntime, Schema } from "effect"
 import { schema } from "shared/schema"
 import { Database } from "./db"
 import { EnvContext, EnvSchema } from "./env"
+import { Auth } from "./auth/main"
 
-export const runtimeLayer = (env: Env) => {
+export const makeRuntime = (env: Env) => {
   const EnvLive = Layer.effect(EnvContext, Schema.decodeUnknown(EnvSchema)(env))
   const DatabaseLive = Layer.sync(Database, () => drizzle(env.DB, { schema }))
 
-  return Layer.mergeAll(EnvLive, DatabaseLive)
+  const AppLive = Layer.empty.pipe(
+    Layer.merge(Auth.Default),
+    Layer.provide(EnvLive),
+    Layer.provideMerge(DatabaseLive),
+  )
+
+  return ManagedRuntime.make(AppLive)
 }
